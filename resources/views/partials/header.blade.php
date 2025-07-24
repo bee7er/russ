c4a988729dce91b6f95e41b503316253ed9153e4
bee7er <contact_bee@yahoo.com>
2025-07-24 20:28:05 +0100

    Add links to the social icons

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,8 +9,8 @@ if (!isset($currentPage)) {
     <div class="hidden-xs hidden-sm col-md-12 col-lg-12 header-block">
         <div class="header-menu-left"><span onclick="gotoPage
         ('home');">Russ Etheridge</span>
-            <div class="contact-email">contact@russetheridge.com</div>
-            <div class=""><img class="contact-social" src="{{config('app.base_url')}}img/social/instagram.png" title="" alt="" /><img class="contact-social" src="{{config('app.base_url')}}img/social/linkedin.png" title="" alt="" /><img class="contact-social" src="{{config('app.base_url')}}img/social/twitter.png" title="" alt="" /><img class="contact-social" src="{{config('app.base_url')}}img/social/youtube.png" title="" alt="" /></div>
+            <div class="contact-email"><a href="mailto:contact@russetheridge.com">contact@russetheridge.com</a></div>
+            <div class=""><a target="_blank" href="https://www.instagram.com/russ_ether/"><img class="contact-social" src="{{config('app.base_url')}}img/social/instagram.png" title="Share on Instagram" alt="" /></a><a target="_blank" href="https://www.linkedin.com/in/russether"><img class="contact-social" src="{{config('app.base_url')}}img/social/linkedin.png" title="Share on linkedin" alt="" /></a><a target="_blank" href="https://twitter.com/russ_ether"><img class="contact-social" src="{{config('app.base_url')}}img/social/twitter.png" title="Share on twitter" alt="" /></a><a target="_blank" href="https://youtube.com/@russ_ether"><img class="contact-social" src="{{config('app.base_url')}}img/social/youtube.png" title="" alt="Check out my Channel" /></a></div>
         </div>
         <div class="header-menu-right"><span id="showreelId" onclick="gotoPage('showreel');"
                                                                            onmouseover="$(this).addClass('white-link-hover');"
